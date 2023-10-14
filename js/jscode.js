54d204e832e5ff71e2b3366e59787ea9873e5c08
TahaMohamed1993 <91631512+TahaMohamed1993@users.noreply.github.com>
2023-10-14 13:43:47 +0300

    first add

---
+++ js/jscode.js
@@ -0,0 +1,14 @@
+// let firstTitel = 'titel1';
+// let firstDiscrption = 'this is discrption';
+// let firstData = '20/2023';
+// const ourContinat = `
+// <div>
+// <h3>${firstTitel}</h3>
+// <p>${firstDiscrption}</p>
+// <span>${firstData}</span>
+// </div>
+
+// `;
+// document.write(ourContinat);
+// document.write(ourContinat);
+// document.write(ourContinat);
